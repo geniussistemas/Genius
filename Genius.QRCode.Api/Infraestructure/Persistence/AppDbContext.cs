b0daad866a28240306d050678bbe720d71986dd6
Alexandre Rodrigues <desenvolvimento1@geniussistemas.com.br>
2025-10-09 14:23:09 -0300

    Feat: Inclusão do AppDbContext no Genius (Core)
    (cherry picked from commit 834f7d30fdbed18b73d38001d0263862483b32d2)

--- Genius.QRCode.Api/Infraestructure/Persistence/AppDbContext.cs
+++ Genius.QRCode.Api/Infraestructure/Persistence/AppDbContext.cs
@@ -1,3 +1,4 @@
+using Genius.Domain.Entities;
 using Genius.QRCode.Api.Domain.Models;
 using Genius.Infraestructure.Persistence;
 using Infraestructure.Persistence;
@@ -5,19 +6,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Genius.QRCode.Api.Infraestructure.Persistence;
 
-public class AppDbContext : DbContext
+public class AppDbContext : Genius.Infraestructure.Persistence.AppDbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    public AppDbContext(DbContextOptions<Genius.Infraestructure.Persistence.AppDbContext> options) : base(options)
     {
     }
 
-    public DbSet<Estacionamento> Estacionamento { get; set; } = null!;
     public DbSet<Pagamento> Pagamento { get; set; } = null!;
     public DbSet<Ticket> Ticket { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        EstacionamentoMappings.Execute(modelBuilder);
+        base.OnModelCreating(modelBuilder);
+
         PagamentoMappings.Execute(modelBuilder);
         TicketMappings.Execute(modelBuilder);
     }
