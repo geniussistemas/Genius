7962d2d7938d6cf0a7a1eedb8323aaa339f8ac5d
Alexandre Rodrigues <desenvolvimento1@geniussistemas.com.br>
2025-10-09 13:51:15 -0300

    Feat: Inclusão de IEstacionamentoRepository

---
+++ Genius/Application/Abstractions/IEstacionamentoRepository.cs
@@ -0,0 +1,9 @@
+namespace Genius.Application.Abstractions;
+
+/// <summary>
+/// Define um contrato para obter dados da unidade/facility.
+/// </summary>
+public interface IEstacionamentoRepository
+{
+    Task<string?> GetIdUnicoUnidadeAsync();
+}
