0b469a346052180e76a2ca8ae92661bf90c529c8
Alexandre Rodrigues <desenvolvimento1@geniussistemas.com.br>
2025-10-10 23:40:39 -0300

    Feat: Inclusão da entidade Ticket e suas classes de mappings e repository

---
+++ Genius/Domain/Entities/Ticket.cs
@@ -0,0 +1,20 @@
+using System.Text.Json.Serialization;
+
+namespace Genius.Domain.Entities;
+
+public class Ticket
+{
+    public long IdTicket { get; set; }
+    public string NumeroTicket { get; set; } = string.Empty;
+    public string? NumeroPlaca { get; set; } = string.Empty;
+    public string IdUnicoEstacionamento { get; set; } = string.Empty;
+    public string? NomeEstacionamento { get; set; } = string.Empty;
+    public string? CodigoRaiaEntrada { get; set; } = string.Empty;
+    public string? NomeRaiaEntrada { get; set; } = string.Empty;
+    public DateTime? DataHoraEntrada { get; set; }
+
+    // Relacionamentos
+    // Não deve permitir a serialização pelo JSON, pois causa recursividade infinita
+    [JsonIgnore]
+    public ICollection<Pagamento>? Pagamentos { get; set; }
+}
