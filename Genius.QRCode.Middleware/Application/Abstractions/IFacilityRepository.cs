af89cfafa8abbaf856e461d1406c3e38a993808e
Alexandre Rodrigues <desenvolvimento1@geniussistemas.com.br>
2025-10-11 00:14:22 -0300

    Feat: Genius.QRCode.Middleware - Eliminaçao de fontes desnecessários

--- Genius.QRCode.Middleware/Application/Abstractions/IFacilityRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace Genius.QRCode.Middleware.Application.Abstractions;
-
-/// <summary>
-/// Define um contrato para obter dados da unidade/facility.
-/// </summary>
-// public interface IEstacionamentoRepository
-// {
-//     Task<string?> GetIdUnicoUnidadeAsync();
-// }
