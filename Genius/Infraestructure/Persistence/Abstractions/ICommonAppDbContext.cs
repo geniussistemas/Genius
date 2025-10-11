d9f53a7a984a52003976c01f6d4774a9ab8b98c0
Alexandre Rodrigues <desenvolvimento1@geniussistemas.com.br>
2025-10-10 23:54:37 -0300

    Feat: Criaçao de CommonAppDbContext compartilhado e classe base CommonRepository (Core)
    (cherry picked from commit e5c1ac7efd8e3556ec00df72b86f36db9c465077)

---
+++ Genius/Infraestructure/Persistence/Abstractions/ICommonAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Genius.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Genius.Infraestructure.Persistence.Abstractions;
+
+public interface ICommonAppDbContext
+{
+    public DbSet<Estacionamento> Estacionamento { get; set; }
+    public DbSet<Config> Config { get; set; }
+    public DbSet<Ticket> Ticket { get; set; }
+    public DbSet<Pagamento> Pagamento { get; set; }
+}
---
+++ Genius/Infraestructure/Persistence/CommonRepository.cs
@@ -0,0 +1,13 @@
+﻿using Genius.Infraestructure.Persistence.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Genius.Infraestructure.Persistence;
+
+public class CommonRepository<TEntity, TContext>(TContext context) : ICommonRepository<TEntity, TContext>
+    where TEntity : class
+    where TContext : DbContext
+{
+    protected readonly TContext Context = context;
+    
+    // Definir métodos comuns das classes de Repository, se houver (incluir em ICommonRepository, se necessário)
+}
