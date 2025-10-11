b95a031cbcf4f71be775cfec9afa4216f56396a8
Alexandre Rodrigues <desenvolvimento1@geniussistemas.com.br>
2025-10-10 23:55:48 -0300

    Feat: Inclusão da entidade Config e suas classes de mappings e repository
    (cherry picked from commit 93b860b1cb2b2a2fb12149d683b0cef53d56ac79)

---
+++ Genius/Application/Abstractions/IConfigRepository.cs
@@ -0,0 +1,11 @@
+﻿using Genius.Domain.Entities;
+
+namespace Genius.Application.Abstractions;
+
+/// <summary>
+/// Contrato para tratar dados de configuração
+/// </summary>
+public interface IConfigRepository
+{
+    Task<Config?> GetConfiguracaoAsync();
+}
