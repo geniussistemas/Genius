7fef884be038ce55a25a742cecd56115f1eafc79
Alexandre Rodrigues <desenvolvimento1@geniussistemas.com.br>
2025-10-13 09:27:52 -0300

    Feat: Ajustes na entidade Estacionamento e seu mapeamento para que trabalhem corretamente com a tabela existente T_ESTACIONAMENTO no que se refere à nullidade de campos da tabela

--- Genius/Domain/Entities/Estacionamento.cs
+++ Genius/Domain/Entities/Estacionamento.cs
@@ -1,27 +1,34 @@
+using System.Runtime.InteropServices.Marshalling;
 using Genius.Domain.ValueObjects;
 
 namespace Genius.Domain.Entities;
 
 public class Estacionamento
 {
+    private string? _idUnicoUnidade;
+    
     public int Id { get; set; }
-    public string? Nome { get; set; }
-    public Endereco Endereco { get; set; } = new();
-    public Telefone Telefone { get; set; } = new();
+    public string Nome { get; set; }
+    public Endereco Endereco { get; set; }
+    public Telefone? Telefone { get; set; }
     public string? Dizeres { get; set; }
     public string? Horario { get; set; }
-    public int CodigoUnidade { get; set; }
+    public int? CodigoUnidade { get; set; }
     public string? NomeUnidade { get; set; }
     public string? RazaoSocial { get; set; }
     public bool Ativo { get; set; }
     public DateTime DataGravacao { get; set; }
     public string? NomeUsuario { get; set; }
-    public int DiaPagamentoMensalidade { get; set; }
-    public decimal JurosMora {  get; set; }
-    public decimal JurosMulta { get; set; }
-    public CCM CCM { get; set; } = new();
-    public CNPJ CNPJ { get; set; } = new();
-    public ImagemExterna Logotipo { get; set; } = new();
-    public int TempoToleranciaMulta { get; set; }
-    public string? IdUnicoUnidade { get; set; }
+    public int? DiaPagamentoMensalidade { get; set; }
+    public decimal? JurosMora {  get; set; }
+    public decimal? JurosMulta { get; set; }
+    public CCM CCM { get; set; }
+    public CNPJ? CNPJ { get; set; }
+    public ImagemExterna? Logotipo { get; set; }
+    public int? TempoToleranciaMulta { get; set; }
+    public string? IdUnicoUnidade
+    {
+        get => _idUnicoUnidade?.Trim();
+        set => _idUnicoUnidade = value;
+    }
 }
