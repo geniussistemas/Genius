bc1d3104af95f7d5261034551a391a077a15564e
Diego O. <doliveira@geniussistemas.com.br>
2025-10-27 11:56:39 -0300

    feat: Criação da classe TabelaPreco

---
+++ Genius/Domain/Entities/TabelaPreco.cs
@@ -0,0 +1,29 @@
+﻿namespace Genius.Domain.Entities
+{
+    public class TabelaPreco
+    {
+        public int Id { get; set; }
+        public int NumTabela { get; set; }
+        public string NomeTabela { get; set; } = string.Empty;
+        public decimal ValorMaximo { get; set; }
+        public decimal TempoToleranciaEntrada { get; set; }
+        public decimal TempoToleranciaEntrePeriodo { get; set; }
+        public decimal? HoraAdicional { get; set; }
+        public decimal? ValorHoraAdicional { get; set; }
+        public decimal? HoraInicioPernoite { get; set; }
+        public decimal? HoraFimPernoite { get; set; }
+        public decimal? HoraInicial { get; set; }
+        public decimal? HoraFinal { get; set; }
+        public bool Ativa { get; set; }
+        public bool Pernoite { get; set; }
+        public decimal ValorMaximoPernoite { get; set; }
+        public string TextoDizeres { get; set; } = string.Empty;
+        public bool Banco { get; set; }
+        public bool Lavagem { get; set; }
+        public DateTime DataHoraGravacao { get; set; }
+        public string? NomeUsuario { get; set; }
+        public bool Repetir { get; set; }
+        public int TempoMinutoInicial { get; set; }
+        public int TempoMinutoFinal { get; set; }
+    }
+}
