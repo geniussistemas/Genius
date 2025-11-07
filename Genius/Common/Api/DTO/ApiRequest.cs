0b4f41504ca487ddc3e991729473ff87f475a075
Diego O. <doliveira@geniussistemas.com.br>
2025-11-07 10:16:42 -0300

    Feat: Implementado ApiRequest e ApiResponse

---
+++ Genius/Common/Api/DTO/ApiRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Genius.Common.Api.DTO
+{
+    public class ApiRequest<TData>
+    {
+        [JsonPropertyName("header")]
+        public RequestHeader? Header { get; set; }
+
+        [JsonPropertyName("data")]
+        public TData? Data { get; set; }
+    }
+
+    public class RequestHeader
+    {
+        [JsonPropertyName("messageType")]
+        public string MessageType { get; set; } = string.Empty;
+
+        [JsonPropertyName("sender")]
+        public string Sender { get; set; } = string.Empty;
+
+        [JsonPropertyName("version")]
+        public string Version { get; set; } = "1.0";
+    }
+}
---
+++ Genius/Common/Api/DTO/ApiResponse.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Genius.Common.Api.DTO
+{
+    public class ApiResponse<TData>
+    {
+        [JsonPropertyName("header")]
+        public ResponseHeader Header { get; set; } = new();
+
+        [JsonPropertyName("data")]
+        public TData? Data { get; set; }
+
+        [JsonPropertyName("result")]
+        public ResponseResult Result { get; set; } = new();
+    }
+
+    public class ResponseHeader
+    {
+        [JsonPropertyName("messageType")]
+        public string MessageType { get; set; } = string.Empty;
+
+        [JsonPropertyName("sender")]
+        public string Sender { get; set; } = "GENIUS";
+
+        [JsonPropertyName("version")]
+        public string Version { get; set; } = "1.0";
+
+    }
+
+    public class ResponseResult
+    {
+        [JsonPropertyName("statusCode")]
+        public int StatusCode { get; set; }
+
+        [JsonPropertyName("code")]
+        public string Code { get; set; } = string.Empty;
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = string.Empty;
+    }
+}
