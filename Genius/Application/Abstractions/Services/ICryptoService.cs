2f483168fd2e0e93600a0366a6eda008c76892e0
Diego O. <doliveira@geniussistemas.com.br>
2025-11-11 17:10:51 -0300

    Feat(Application): Criado a interface para ICryptoService

---
+++ Genius/Application/Abstractions/Services/ICryptoService.cs
@@ -0,0 +1,10 @@
+﻿namespace Genius.Application.Abstractions.Services;
+
+public interface ICryptoService
+{
+    byte[] EncryptData(string data);
+    byte[] EncryptData(string data, string key);
+
+    string DecryptData(byte[] encryptedData);
+    string DecryptData(byte[] encryptedData, string key);
+}
