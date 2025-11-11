3352b89a4b6059ae5b27ed4e0accec4f00301b46
Diego O. <doliveira@geniussistemas.com.br>
2025-11-11 11:03:55 -0300

    Feat: Criação das interfaces ITabelaPrecoRepository e IObterTabelasPrecoSimplificadaUseCase

---
+++ Genius/Application/Abstractions/TabelaPreco/IObterTabelasPrecoSimplificadaUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Genius.Application.DTO;
+using Genius.Common.Lib.Results;
+
+namespace Genius.Application.Abstractions.TabelaPreco
+{
+    public interface IObterTabelasPrecoSimplificadaUseCase
+    {
+        Task<Result<List<TabelaPrecoSimplificada>?>> ExecutarAsync();
+    }
+}
---
+++ Genius/Application/Abstractions/TabelaPreco/ITabelaPrecoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Genius.Application.DTO;
+
+namespace Genius.Application.Abstractions.TabelaPreco
+{
+    public interface ITabelaPrecoRepository
+    {
+        Task<List<TabelaPrecoSimplificada>?> ObterTabelasPrecoSimplificadasAsync(CancellationToken cancellationToken = default);
+    }
+}
