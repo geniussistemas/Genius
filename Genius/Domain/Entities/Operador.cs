0c4ad8e8557eab5ccdfe7a1d5df957087b6a94c8
Diego O. <doliveira@geniussistemas.com.br>
2025-11-11 17:09:16 -0300

    Feat(Domain): Criado a entidade Operador.

---
+++ Genius/Domain/Entities/Operador.cs
@@ -0,0 +1,19 @@
+﻿namespace Genius.Domain.Entities
+{
+    public class Operador
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public bool Ativo { get; set; }
+        public DateTime DataHoraGravacao { get; set; }
+        public int? AlterarSenha { get; set; }
+        public string? Ip { get; set; }
+        public byte CodigoSkin { get; set; }
+        public bool SugerirAberturaCaixa { get; set; }
+        public string Matricula { get; set; } = string.Empty;
+        public int? PerfilId { get; set; }
+
+    }
+}
