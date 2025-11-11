61b5f0479d8e39846d966c319a9e0271859eca5e
Diego O. <doliveira@geniussistemas.com.br>
2025-11-11 17:15:41 -0300

    Feat: Implementado a classe ObterListaUsuariosUseCase

---
+++ Genius/Application/UseCases/Operador/ObterListaUsuariosUseCase.cs
@@ -0,0 +1,33 @@
+﻿using Genius.Application.Abstractions.Caixa;
+using Genius.Application.Abstractions.Operador;
+using Genius.Common.Lib.Results;
+
+namespace Genius.Application.UseCases.Operador
+{
+    public class ObterListaUsuariosUseCase(IOperadorRepository operadorRepo, ITerminalCaixaRepository caixaRepo) : IObterListaUsuariosUseCase
+    {
+        public async Task<Result<List<string>>> ExecutarAsync(int numeroTerminal)
+        {
+            try
+            {
+                if (!await caixaRepo.NumeroTerminalExisteAsync(numeroTerminal))
+                {
+                    return Error.NotFound(
+                        "CAIXA.TERMINAL_INEXISTENTE",
+                        "Não foi possível localizar os logins de usuários para caixa através do número de terminal fornecido."
+                    );
+                }
+
+                var result = await operadorRepo.ObterUsuariosAsync();
+
+                if (result is not null) return result;
+
+                return new List<string>();
+            }
+            catch (Exception)
+            {
+                return Error.Failure("INTERNAL_SERVER_ERROR", "Ocorreu um erro interno ao processar a solicitação.");
+            }
+        }
+    }
+}
---
+++ Genius/Tests/Genius.Application.Tests/UseCases/Operador/ObterListaUsuariosUseCaseTests.cs
@@ -0,0 +1,114 @@
+﻿using Genius.Application.Abstractions.Caixa;
+using Genius.Application.Abstractions.Operador;
+using Genius.Application.UseCases.Operador;
+using Genius.Common.Lib.Results;
+using Moq;
+using Shouldly;
+
+namespace Genius.Application.Tests.UseCases.Operador
+{
+    public class ObterListaUsuariosUseCaseTests
+    {
+        [Fact]
+        public async Task ObterListaUsuario_QuandoTerminalNaoCadastrado_DeveRetornarNaoEncontrado()
+        {
+            const int numeroTerminal = 99;
+
+            var caixaRepoMock = new Mock<ITerminalCaixaRepository>();
+            var operadorRepoMock = new Mock<IOperadorRepository>();
+
+            caixaRepoMock.Setup(c => c.NumeroTerminalExisteAsync(numeroTerminal)).ReturnsAsync(false);
+
+            var listaOperadorUseCase = new ObterListaUsuariosUseCase(operadorRepoMock.Object, caixaRepoMock.Object);
+
+            var result = await listaOperadorUseCase.ExecutarAsync(numeroTerminal);
+
+            result.IsFailure.ShouldBeTrue();
+            result.Errors.ShouldHaveSingleItem();
+            result.Errors[0].Type.ShouldBe(ErrorType.NotFound);
+        }
+
+        [Fact]
+        public async Task ObterListaUsuario_QuandoDadosValidos_DeveRetornarSucesso()
+        {
+            const int numeroTerminal = 1;
+            List<string> logins = ["ADMIN", "MANUTENCAO"];
+            var caixaRepoMock = new Mock<ITerminalCaixaRepository>();
+            var operadorRepoMock = new Mock<IOperadorRepository>();
+            caixaRepoMock.Setup(c => c.NumeroTerminalExisteAsync(numeroTerminal)).ReturnsAsync(true);
+            operadorRepoMock.Setup(o => o.ObterUsuariosAsync(It.IsAny<CancellationToken>())).ReturnsAsync(logins);
+
+            var listaOperadorUseCase = new ObterListaUsuariosUseCase(operadorRepoMock.Object, caixaRepoMock.Object);
+
+            var result = await listaOperadorUseCase.ExecutarAsync(numeroTerminal);
+
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.ShouldNotBeNull();
+            result.Value.Count.ShouldBe(2);
+            result.Value.ShouldBeEquivalentTo(logins);
+        }
+
+        [Fact]
+        public async Task ObterListaUsuario_QuandoHouverErroInternoNumeroTerminalExiste_DeveRetornarFalha()
+        {
+            const int numeroTerminal = 1;
+
+            var caixaRepoMock = new Mock<ITerminalCaixaRepository>();
+            var operadorRepoMock = new Mock<IOperadorRepository>();
+
+            caixaRepoMock.Setup(c => c.NumeroTerminalExisteAsync(numeroTerminal)).ThrowsAsync(new Exception("Houve uma falha interna"));
+
+            var listaOperadorUseCase = new ObterListaUsuariosUseCase(operadorRepoMock.Object, caixaRepoMock.Object);
+
+            var result = await listaOperadorUseCase.ExecutarAsync(numeroTerminal);
+
+            result.IsFailure.ShouldBeTrue();
+            result.Errors.ShouldNotBeNull();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors[0].Type.ShouldBe(ErrorType.Failure);
+            result.Errors[0].Message.ShouldBe("Ocorreu um erro interno ao processar a solicitação.");
+        }
+
+
+        [Fact]
+        public async Task ObterListaUsuario_QuandoHouverErroInternoRepository_DeveRetornarFalha()
+        {
+            const int numeroTerminal = 1;
+
+            var caixaRepoMock = new Mock<ITerminalCaixaRepository>();
+            var operadorRepoMock = new Mock<IOperadorRepository>();
+
+            caixaRepoMock.Setup(c => c.NumeroTerminalExisteAsync(numeroTerminal)).ReturnsAsync(true);
+            operadorRepoMock.Setup(o => o.ObterUsuariosAsync(It.IsAny<CancellationToken>())).Throws(new Exception("Houve um erro interno"));
+
+            var listaOperadorUseCase = new ObterListaUsuariosUseCase(operadorRepoMock.Object, caixaRepoMock.Object);
+
+            var result = await listaOperadorUseCase.ExecutarAsync(numeroTerminal);
+
+            result.IsFailure.ShouldBeTrue();
+            result.Errors.ShouldNotBeNull();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors[0].Type.ShouldBe(ErrorType.Failure);
+            result.Errors[0].Message.ShouldBe("Ocorreu um erro interno ao processar a solicitação.");
+        }
+
+        [Fact]
+        public async Task ObterListaUsuario_QuandoNaoHouverUsuarios_DeveRetornaSucessoComUmaListaVazia()
+        {
+            const int numeroTerminal = 1;
+            List<string>? logins = null;
+            var caixaRepoMock = new Mock<ITerminalCaixaRepository>();
+            var operadorRepoMock = new Mock<IOperadorRepository>();
+            caixaRepoMock.Setup(c => c.NumeroTerminalExisteAsync(numeroTerminal)).ReturnsAsync(true);
+            operadorRepoMock.Setup(o => o.ObterUsuariosAsync(It.IsAny<CancellationToken>())).ReturnsAsync(logins);
+
+            var listaOperadorUseCase = new ObterListaUsuariosUseCase(operadorRepoMock.Object, caixaRepoMock.Object);
+
+            var result = await listaOperadorUseCase.ExecutarAsync(numeroTerminal);
+
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.ShouldNotBeNull();
+            result.Value.ShouldBeEmpty();
+        }
+    }
+}
