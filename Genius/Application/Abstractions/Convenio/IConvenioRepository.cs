acb55c7fb9dce409b792a730a84ebe1a91d0b89b
Diego O. <doliveira@geniussistemas.com.br>
2025-11-11 11:01:30 -0300

    Feat: Criação das interfaces IConvenioRepository e IObterConveniosSimplificadoUseCase

---
+++ Genius/Application/Abstractions/Convenio/IConvenioRepository.cs
@@ -0,0 +1,9 @@
+﻿using Genius.Application.DTO;
+
+namespace Genius.Application.Abstractions.Convenio
+{
+    public interface IConvenioRepository
+    {
+        Task<List<ConvenioSimplificado>> ObterConvenioSimplificadosAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Genius/Application/Abstractions/Convenio/IObterConveniosSimplificadoUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Genius.Application.DTO;
+using Genius.Common.Lib.Results;
+
+namespace Genius.Application.Abstractions.Convenio
+{
+    public interface IObterConveniosSimplificadoUseCase
+    {
+        Task<Result<List<ConvenioSimplificado>>> ExecutarAsync();
+    }
+}
