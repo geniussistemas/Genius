41f97593eb7ab6014873a86d6dd6fc166d77ee73
Diego O. <doliveira@geniussistemas.com.br>
2025-11-11 17:10:13 -0300

    Feat(Application): Criado as interfaces IObterListaUsuariosUseCase e IOperadorRepository

---
+++ Genius/Application/Abstractions/Operador/IObterListaUsuariosUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Genius.Common.Lib.Results;
+
+namespace Genius.Application.Abstractions.Operador
+{
+    public interface IObterListaUsuariosUseCase
+    {
+        Task<Result<List<string>>> ExecutarAsync(int numeroTerminal);
+    }
+}
---
+++ Genius/Application/Abstractions/Operador/IOperadorRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Genius.Application.Abstractions.Operador
+{
+    public interface IOperadorRepository
+    {
+        Task<List<string>?> ObterUsuariosAsync(CancellationToken cancellationToken = default);
+        Task<string> ObterSenhaPeloLoginAsync(string login, CancellationToken cancellationToken = default);
+        Task<Domain.Entities.Operador?> ObterOperadorPeloLoginAsync(string login, CancellationToken cancellationToken = default);
+    }
+}
