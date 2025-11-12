112bb6b05e8a35c3f3fea313ff62f335b08d1e06
Diego O. <doliveira@geniussistemas.com.br>
2025-11-12 15:58:56 -0300

    Feat: Implementado classe OperadorPerfil, OperadorPerfilRepository.

---
+++ Genius/Application/Abstractions/Operador/IOperadorPerfilRepository.cs
@@ -0,0 +1,9 @@
+﻿using Genius.Domain.Entities;
+
+namespace Genius.Application.Abstractions.Operador
+{
+    public interface IOperadorPerfilRepository
+    {
+        Task<OperadorPerfil?> ObterPerfilPeloIdAsync(int id);
+    }
+}
---
+++ Genius/Domain/Entities/OperadorPerfil.cs
@@ -0,0 +1,10 @@
+﻿namespace Genius.Domain.Entities
+{
+    public class OperadorPerfil
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public DateTime DataHoraGravacao { get; set; }
+
+    }
+}
